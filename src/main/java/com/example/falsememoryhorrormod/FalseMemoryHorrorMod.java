265c9582b102ba9a9bf188d30f92e6669e592c45
Festuwu <floppowskafloppa@gmail.com>
2025-07-18 17:34:17 +0200

    Update FalseMemoryHorrorMod.java

--- src/main/java/com/example/falsememoryhorrormod/FalseMemoryHorrorMod.java
+++ src/main/java/com/example/falsememoryhorrormod/FalseMemoryHorrorMod.java
@@ -3,6 +3,8 @@ package com.example.falsememoryhorrormod;
 import com.example.falsememoryhorrormod.SoundRegistry;
 import com.example.falsememoryhorrormod.world.gen.ModFeatures;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
+import net.minecraft.server.world.ServerWorld;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,5 +18,10 @@ public class FalseMemoryHorrorMod implements ModInitializer {
         SoundRegistry.registerSounds();
         ModFeatures.registerFeatures();
         MobDisappearanceManager.initialize();
+        ServerTickEvents.END_SERVER_TICK.register(server -> {
+            for (ServerWorld world : server.getWorlds()) {
+                MobDisappearanceManager.tick(world);
+            }
+        });
     }
 } 
